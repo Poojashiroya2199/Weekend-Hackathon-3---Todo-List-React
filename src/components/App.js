ab0aca7237a714ea532891719bff86395ca52359
pooja shiroya <67506850+Poojashiroya2199@users.noreply.github.com>
2020-10-31 18:18:41 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,22 @@
 import React from "react";
 import "./../styles/App.css";
 
-function App() 
-{
-	return (
-	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
-	</div>
-	);
+function App() {
+  const handleClick = () => {
+    console.log("clicked");
+  };
+  return (
+    <>
+      <div id="main">
+        //Do not alter main div //Please do not alter the functional component
+        as tests depend on the type of component.
+      </div>
+      <input type="text" id="task" />
+      <button id="btn" onClick={handleClick}>
+        Add
+      </button>
+    </>
+  );
 }
-
 
 export default App;
